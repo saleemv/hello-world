95744dfe70ac2387191e2fe7f275627fe09ad219
saleemv <37408971+saleemv@users.noreply.github.com>
2018-03-22 02:39:56 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,28 @@
+variable "region" {
+  default = "ap-south-1"
+  description = "AWS region code"
+}
+
+variable "vpc_name"   {
+  description = "Name for your vpc"
+}
+
+variable "vpc_cidr"  {
+  description = "CIDR block for vpc subnet"
+}
+
+variable "az" {
+  description = "Availability Zone"
+}
+
+variable "jdk" {
+  description = "Location of java 8 jdk"
+}
+
+variable "git" {
+  description = "Location of dockerfile and other files from git"
+}
+
+variable "creds" {
+  description = "Location of the AWS key and secret"
+}
